fdf0274ae13a7bae536131b182e03a2669619b6d
Kiavad <marc_monnin@yahoo.com>
2014-04-19 14:44:20 -0400

    fix code so it works

---
+++ Test.R
@@ -0,0 +1,18 @@
+> source('C:/Data Science/R Class/ProgrammingAssignment2/ProgrammingAssignment2/cachematrix.R')
+> cacheSolve(x<-makeCacheMatrix(mymatrix))
+[,1] [,2]
+[1,]   -2  1.5
+[2,]    1 -0.5
+> makeCacheMatrix(mymatrix))
+
+> cacheSolve(x)
+getting cached data
+[,1] [,2]
+[1,]   -2  1.5
+[2,]    1 -0.5
+> 
+}
+
+        
+        
+        
